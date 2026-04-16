de0fffeb8bf03fae2cab8f9547a107c44187ac6e
Muhammadqodir <muxaxolmatov0@gmail.com>
2026-04-16 14:21:07 +0500

    one-question reply

---
+++ main.js
@@ -0,0 +1,9 @@
+// 1-masala sonning juft yoki toq son ekanligini aniqlovchi dastur yozing
+
+// let son = +prompt("son kiriting");
+
+// if (son % 2 === 0) {
+//   console.log("Bu juft son");
+// } else {
+//   console.log("Bu toq son");
+// }
